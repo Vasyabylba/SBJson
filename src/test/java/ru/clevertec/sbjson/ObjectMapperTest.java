ef367414d0f5e561384da8f78e62b5ec785151e5
Vasiliy Yalovchik <vasyabylba@gmail.com>
2024-10-07 03:55:10 +0300

    add tests for deserialization

--- src/test/java/ru/clevertec/sbjson/ObjectMapperTest.java
+++ src/test/java/ru/clevertec/sbjson/ObjectMapperTest.java
@@ -1,13 +1,17 @@
 package ru.clevertec.sbjson;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import domain.Customer;
+import ru.clevertec.sbjson.exception.JsonProcessingException;
+import ru.clevertec.sbjson.exception.MismatchedInputException;
 import util.TestData;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ObjectMapperTest {
     com.fasterxml.jackson.databind.ObjectMapper jacksonObjectMapper;
@@ -17,12 +21,13 @@ class ObjectMapperTest {
         jacksonObjectMapper = new com.fasterxml.jackson.databind.ObjectMapper();
         jacksonObjectMapper.registerModule(new JavaTimeModule());
         jacksonObjectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        jacksonObjectMapper.disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE);
         objectMapper = new ObjectMapper();
     }
 
     @Test
     @SneakyThrows
-    void toJson() {
+    void shouldGetJson() {
         //given
         Customer customer = TestData.generateCustomer();
         String expected = jacksonObjectMapper.writeValueAsString(customer);
@@ -32,5 +37,28 @@ class ObjectMapperTest {
 
         //then
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldGetObject() throws JsonProcessingException, com.fasterxml.jackson.core.JsonProcessingException {
+        //given
+        String json = TestData.generateCustomerJson();
+        Customer expected = jacksonObjectMapper.readValue(json, Customer.class);
+
+        //when
+        Customer actual = objectMapper.toObject(json, Customer.class);
+
+        //then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldThrowMismatchedInputException_whenMismatchedInput() {
+        //given
+        String json = TestData.generateInvalidCustomerJson();
+
+        //when, then
+        assertThrows(MismatchedInputException.class, () ->
+                objectMapper.toObject(json, Customer.class));
     }
 }
